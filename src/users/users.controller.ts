e2475f9a4fb111bee5d6ae8ba64fd1fee91f8b8c
vhs0007 <itzvalegamer12@gmail.com>
2026-02-16 22:11:24 -0300

    Move JWT auth guard to users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { UsersService } from './users.service';
 import type { CreateUserDto } from './dto/create-user.dto';
 import type { UpdateUserDto } from './dto/update-user.dto';
-import { AuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
+import { AuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 
 @Controller('users')
 @UseGuards(AuthGuard) 
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AuthModule } from '../auth/auth.module';
+import { AuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthGuard],
 })
 export class UsersModule {}
