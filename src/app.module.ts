515eee8853a4013cbde70f0d4088ab6eff83a194
CyruelOverride <brxxcanario555@gmail.com>
2026-02-25 00:37:40 -0300

    Resuelto conflicto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ActivitiesModule } from './activities/activities.module';
 import { FacilitiesModule } from './facilities/facilities.module';
+import { MembershipModule } from './membership/membership.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ActivitiesModule, FacilitiesModule],
+  imports: [AuthModule, UsersModule, ActivitiesModule, FacilitiesModule, MembershipModule,],
   controllers: [AppController],
   providers: [AppService],
 })
