ee0fb806e526c386cd7a25000d96b5b071e3b66f
CyruelOverride <brxxcanario555@gmail.com>
2026-03-04 16:24:22 -0300

    Repo activites

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { ActivitiesController } from './activities.controller';
+import { ActivitiesRepository } from './activities.repository';
 
 @Module({
   controllers: [ActivitiesController],
-  providers: [ActivitiesService],
+  providers: [ActivitiesService, ActivitiesRepository],
 })
 export class ActivitiesModule {}
