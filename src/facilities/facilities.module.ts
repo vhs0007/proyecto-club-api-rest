7912d5a3ddfd43420953f90ae8c8e22bab719e8b
vhs0007 <itzvalegamer12@gmail.com>
2026-03-05 20:41:25 -0300

    no mas mutantes

--- src/facilities/facilities.module.ts
+++ src/facilities/facilities.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { FacilitiesService } from './facilities.service';
 import { FacilitiesController } from './facilities.controller';
 import { FacilitiesRepository } from './repository/facilities.repository.impl';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [FacilitiesController],
   providers: [FacilitiesService, FacilitiesRepository],
 })
