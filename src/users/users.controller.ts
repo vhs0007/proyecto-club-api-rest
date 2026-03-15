1cab7beeddacff32f1f0ed5ea0155a0e79f68333
VHS0007 <itzvalegamer12@gmail.com>
2026-03-14 22:03:24 -0300

    respuesta para users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import type { UserEntity } from './users.service';
+import { UserResponseDto } from './dto/response/user.response.dto';
+import { CreateUserDto } from './dto/request/create-user.request.dto';
+import { UpdateUserDto } from './dto/request/update-user.request.dto';
 import { AuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @ApiTags('Users')
@@ -16,32 +16,32 @@ export class UsersController {
   @Post()
   @ApiOperation({ summary: 'Crear usuario' })
   @ApiBody({ type: CreateUserDto })
-  create(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
+  create(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
   @ApiOperation({ summary: 'Obtener todos los usuarios' })
-  findAll(): Promise<UserEntity[]> {
+  findAll(): Promise<UserResponseDto[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Obtener usuario por ID' })
-  findOne(@Param('id') id: string): Promise<UserEntity> {
+  findOne(@Param('id') id: string): Promise<UserResponseDto> {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
   @ApiOperation({ summary: 'Actualizar usuario' })
   @ApiBody({ type: UpdateUserDto })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<UserEntity> {
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<UserResponseDto> {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Eliminar usuario' })
-  remove(@Param('id') id: string): Promise<UserEntity> {
+  remove(@Param('id') id: string): Promise<UserResponseDto> {
     return this.usersService.remove(+id);
   }
 }
