a84fdd1c015f2b8a7536f05a6dd59aa09ee56196
vhs0007 <itzvalegamer12@gmail.com>
2026-04-05 21:54:09 -0300

    coso

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
     if (loginRequest.email === 'admin@admin.com' && loginRequest.password === 'admin') {
       const payload = { sub: 0, email: 'admin@admin.com', role: 'admin' };
       const accessToken = this.jwtService.sign(payload);
-      return { accessToken, role: 'admin', clubId: 1 };
+      return { accessToken, role: 'admin', clubId: loginRequest.clubId };
     }
     const user = await this.prisma.users.findFirst({
       where: { email: loginRequest.email },
